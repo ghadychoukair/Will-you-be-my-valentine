f98b8105891d69d4a1c04fcc3e8eb41660654503
ghadychoukair <ghadychoukair@gmail.com>
2026-02-07 03:09:45 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// I assume 'wait...no? 😲' is related to some hover event
-// Example modification within an event listener
-
-document.querySelector('.hover-element').addEventListener('mouseover', function() {
-    this.title = 'wait...no? 😲';
-});
-
-// Further code here...
